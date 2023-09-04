637ef291621111e9e21601304bcb183710dc053f
Evgeniy <ramzik47@gmail.com>
2023-09-04 23:49:02 +0500

    seminar1 task 0,1,3,5

---
+++ seminar1task0/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+//Считываем данные с консоли
+string? inputNum = Console.ReadLine();//??"0";
+//Проверяем, чтобы данные были не пустыми
+if(inputNum!=null)
+{
+    // //Парсим введенное число
+    // int num = int.Parse(inputNum);
+    // //Находим квадрат числа
+    // int res = num*num;
+    // // Выводим данные в консоль 
+    // Console.WriteLine(res);
+
+    Console.WriteLine("Квадрат числа: "+(int)Math.Pow(int.Parse(inputNum),2));
+}
