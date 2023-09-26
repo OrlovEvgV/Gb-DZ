e0b959468ebeab17a4407a644c6261816e818286
Evgeniy <t40165984@gmail.com>
2023-09-26 20:38:13 +0500

    home work Sem5

---
+++ Sem5Task38/Program.cs
@@ -0,0 +1,38 @@
+﻿double[] GenArray(int length, int lowBorder, int highBorder)
+{
+    double[] arr = new double[length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(lowBorder, highBorder + 1) + random.NextDouble();
+    }
+    return arr;
+}
+
+double MinMax(double[] arr)
+{
+    double min = arr[0];
+    double max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]<min)
+        {
+            min = arr[i];
+        }
+        if(arr[i]>max)
+        {
+            max = arr[i];
+        }
+    }
+    double res = max - min;
+    return res;
+}
+
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+double[] arr = GenArray(20, 0, 99);
+double res = MinMax(arr);
+PrintResult("" + res);
