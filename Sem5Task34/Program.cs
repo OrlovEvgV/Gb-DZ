b5a47d2af9488884853a719d651c20fd4aafee31
Evgeniy <t40165984@gmail.com>
2023-09-26 16:09:15 +0500

    home work Sem5

---
+++ Sem5Task34/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] GenArray(int length, int lowBorder, int highBorder)
+{
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(lowBorder, highBorder + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + " ]");
+    System.Console.WriteLine();
+}
+
+int CountElements(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        if(array[i]%2==0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int[] array = GenArray(1000, 100, 999);
+int count = CountElements(array);
+PrintArray(array);
+PrintResult("Количество четных элементов: " + count);
+
+
