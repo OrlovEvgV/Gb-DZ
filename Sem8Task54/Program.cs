f330bef50aa54ccf75ffa12386040b63ec33b675
Evgeniy <ramzik47@gmail.com>
2023-10-02 00:32:50 +0500

    home work Sem8

---
+++ Sem8Task54/Program.cs
@@ -0,0 +1,63 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+int[,] Fill2DArray(int countRow, int countColumn)
+{
+    System.Random rnd = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i,j] = rnd.Next(1,9);
+        }
+    }
+
+    return array2D;
+}
+
+void Print2DArray(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int numberOfRows = ReadData("введите количество строк ");
+int numberOfColumns = ReadData("введите количество столбцов ");
+int[,] arr2D = Fill2DArray(numberOfRows, numberOfColumns);
+Print2DArray(arr2D);
+SortToLower(arr2D);
+Console.WriteLine("---------------------------------");
+Print2DArray(arr2D);
---
+++ test/Program.cs
@@ -0,0 +1,40 @@
+﻿int[,] CreateMatrix(int m, int n, int k)
+{
+    int[,] matrix = new int[m, n];
+    int count = 0;
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            count = count + k;
+            matrix[i, j] = count;
+        }
+    }
+    return matrix;
+}
+
+void Print2DArray(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}    
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+int m = ReadData(" ");
+int n = ReadData(" ");
+int k = ReadData(" ");
+int[,] matrix = CreateMatrix(m, n, k);
+Print2DArray(matrix);
